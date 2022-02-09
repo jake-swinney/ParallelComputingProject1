94a66bca0c3ef08485f579912769c9c5b5e82e62
ryanmobley <ryanmobley024@gmail.com>
2022-02-09 17:59:36 -0500

    MORE formatting fixes

--- SThread.java
+++ SThread.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.net.*;
 import java.lang.Exception;
 
-	
-public class SThread extends Thread 
+
+public class SThread extends Thread
 {
 	private Object [][] RTable; // routing table
 	private PrintWriter out, outTo; // writers (for writing back to the machine and to destination)
-   private BufferedReader in; // reader (for reading from the machine connected to)
+	private BufferedReader in; // reader (for reading from the machine connected to)
 	private String inputLine, outputLine, destination, addr; // communication strings
 	private Socket outSocket; // socket for communicating with a destination
 	private int ind; // indext in the routing table
@@ -19,11 +19,11 @@ public class SThread extends Thread
 		in = new BufferedReader(new InputStreamReader(toClient.getInputStream()));
 		RTable = Table;
 		addr = toClient.getInetAddress().getHostAddress();
-		RTable[index][0] = addr; // IP addresses 
+		RTable[index][0] = addr; // IP addresses
 		RTable[index][1] = toClient; // sockets for communication
 		ind = index;
 	}
-	
+
 	// Run method (will run for each machine that connects to the ServerRouter)
 	public void run()
 	{
@@ -43,9 +43,9 @@ public class SThread extends Thread
 			{
 				System.out.println("Thread interrupted");
 			}
-		
+
 			// loops through the routing table to find the destination
-			for ( int i=0; i<10; i++) 
+			for ( int i=0; i<10; i++)
 			{
 				if (destination.equals((String) RTable[i][0]))
 				{
@@ -54,22 +54,22 @@ public class SThread extends Thread
 					outTo = new PrintWriter(outSocket.getOutputStream(), true); // assigns a writer
 				}
 			}
-		
-			// Communication loop	
-			while ((inputLine = in.readLine()) != null) 
+
+			// Communication loop
+			while ((inputLine = in.readLine()) != null)
 			{
 				System.out.println("Client/Server said: " + inputLine);
 				if (inputLine.equals("Bye.")) // exit statement
-						break;
+					break;
 				outputLine = inputLine; // passes the input from the machine to the output string for the destination
-				
+
 				if (outSocket != null)
-				{				
+				{
 					outTo.println(outputLine); // writes to the destination
-				}			
-			}// end while		 
+				}
+			}// end while
 		}// end try
-		catch (IOException e) 
+		catch (IOException e)
 		{
 			System.err.println("Could not listen to socket.");
 			System.exit(1);
